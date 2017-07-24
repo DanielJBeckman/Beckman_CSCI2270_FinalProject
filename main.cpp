7077f3e35e5823bcf6ca99fa73d48673fb9d9860
Daniel Beckman <djbeckman42@gmail.com>
2017-07-24 11:26:59 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,48 @@
 //Main Driver
+#include <iostream>
+#include "Route.h"
+//#include "Route.cpp"
+
+using namespace std;
+
+
+int main()
+{
+    int choice = 0;
+   
+  
+    
+    while(choice != 4){
+        
+        cout << "==========Main Menu==========" << endl;
+        cout << "1. Print route" << endl;
+        cout << "2. Find airfare" << endl;
+        cout << "3. Run simulation" << endl;
+        cout << "4. Exit" << endl;
+        cout << "5. Print graph" << endl;
+        cout << "6. Print Tree" << endl;
+        
+        cin >> choice;
+        
+        switch(choice){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+        }
+        }
+        
+    
+    
+    
+    
+    return 0;
+}
