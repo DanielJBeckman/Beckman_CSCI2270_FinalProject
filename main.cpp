e8e956e6a6f6ba888903664778e1abafc5b9a493
Daniel Beckman <djbeckman42@gmail.com>
2017-07-27 06:20:01 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,28 @@
-//Main Driver
 #include <iostream>
-#include "Route.h"
-//#include "Route.cpp"
+#include <vector>
+//#include "Functions.cpp"
+#include "Functions.hpp"
 
 using namespace std;
+
 
 
 int main()
 {
     int choice = 0;
    
-  
+    Route R;
+    
+    R.BuildG();
+    R.BuildT();
+    //R.PrntG();
+    //R.PrntR(R.sim);
+
     
     while(choice != 4){
         
         cout << "==========Main Menu==========" << endl;
-        cout << "1. Print route" << endl;
+        cout << "1. Find route" << endl;
         cout << "2. Find airfare" << endl;
         cout << "3. Run simulation" << endl;
         cout << "4. Exit" << endl;
@@ -23,24 +30,26 @@ int main()
         cout << "6. Print Tree" << endl;
         
         cin >> choice;
+        cin.clear();
+        cin.ignore();
         
         switch(choice){
-            case 1:
+            case 1: R.PrntR(R.sim);
                 break;
-            case 2:
+            case 2: R.getFare();
                 break;
-            case 3:
+            case 3: R.Scenario();
                 break;
-            case 4:
+            case 4: cout << "Goodbye!" << endl;
                 break;
-            case 5:
+            case 5: R.PrntG();
                 break;
             case 6:
                 break;
         }
         }
         
-    
+ 
     
     
     
